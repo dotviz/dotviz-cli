e7a5313f080a5c85f9758ed98be4388d3b0a13a0
vitalnodo <vitalnodo@outlook.com>
2025-11-24 14:41:33 +0200

    remove root.zig

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,4 @@
 const std = @import("std");
-const dotviz_cli = @import("dotviz_cli");
 const WebView = @import("webview").WebView;
 
 pub fn main() !void {
@@ -8,23 +7,4 @@ pub fn main() !void {
     try w.setHtml("Thanks for using webview!");
     try w.run();
     try w.destroy();
-}
-
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
 }
